9eaec30f93edc07b1e277adeb92b1a4138c40268
Nurlelalela <nurlelalela20117@gmail.com>
2025-08-28 12:49:02 +0700

    first commit

---
+++ lela/bin/lela.dart
@@ -0,0 +1,5 @@
+import 'package:lela/lela.dart' as lela;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lela.calculate()}!');
+}
---
+++ lela/test/lela_test.dart
@@ -0,0 +1,8 @@
+import 'package:lela/lela.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
