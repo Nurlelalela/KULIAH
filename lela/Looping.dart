0c13e7d35ac880b62682fb4896f82eadd54a4434
Nurlelalela <nurlelalela20117@gmail.com>
2025-10-04 11:51:28 +0700

    tambahkan tugas aplikasi restoran

---
+++ lela/Looping.dart
@@ -0,0 +1,21 @@
+// import 'dart:io';
+// void main() {
+//   for (int i = 1; i < 1000; i++) {
+//     print("Selamat Datang");
+//   }
+
+// }
+
+
+// void main(){
+//   for (int i = 1; i <= 10; i++)
+//   if ( i % 2 == 0) {
+//     print(i);
+//   }
+// }
+
+//break
+void main() {
+  int number = 10 ~/ 0;
+  print(number);
+} 
---
+++ lela/operator.dart
@@ -0,0 +1,33 @@
+ // aritmatika operator
+
+//  void main() {
+//     String  username  = "user";
+
+//     int password = 123;
+
+//     print(username == "user" && password == 1235);
+//  }
+
+
+  void main() {
+    String username = "Nurlela";
+
+    int password = 07352311048;
+
+    if (username == "Nurlela" && password == 07352311048) {
+      print("Login Berhasil");
+      } else {
+        print("Login Gagal");
+      }
+
+    }
+
+
+  // import "dart:io";
+
+  //   void main() {
+  //     stdout.write ("Masukan nama :");
+  //     String ? nama = stdin.readLineSync();
+
+  //     print("nama saya : $nama");
+  //   }
